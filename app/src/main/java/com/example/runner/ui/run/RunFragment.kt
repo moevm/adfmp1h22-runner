1b96027c2696f6f7ef8443bbba5bd55b5983d18c
Shchuka Alexander <42916374+AleksandrShuka@users.noreply.github.com>
2022-02-23 13:30:48 +0300

    profile page

--- app/src/main/java/com/example/runner/ui/run/RunFragment.kt
+++ app/src/main/java/com/example/runner/ui/run/RunFragment.kt
@@ -4,35 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.runner.databinding.FragmentRunBinding
 
 class RunFragment : Fragment() {
-
     private var _binding: FragmentRunBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        val runViewModel =
-                ViewModelProvider(this).get(RunViewModel::class.java)
 
         _binding = FragmentRunBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textRun
-        runViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
