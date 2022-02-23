6b97008b3d489a4370b6ea27b9e12a20630de42a
Maria Nikolaeva <42906376+Nyashechka@users.noreply.github.com>
2022-02-23 15:33:35 +0300

    delete tutorial's code

--- app/src/main/java/com/example/runner/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/runner/ui/statistics/StatisticsFragment.kt
@@ -4,35 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.runner.databinding.FragmentStatisticsBinding
 
 class StatisticsFragment : Fragment() {
 
     private var _binding: FragmentStatisticsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-                ViewModelProvider(this).get(StatisticsViewModel::class.java)
-
         _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
