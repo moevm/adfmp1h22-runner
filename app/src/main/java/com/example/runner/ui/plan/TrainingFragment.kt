6f7764b9317762292ad57652a4f945fb931a3d61
Maria Nikolaeva <42906376+Nyashechka@users.noreply.github.com>
2022-02-24 18:04:30 +0300

    change training fragment

--- app/src/main/java/com/example/runner/ui/plan/TrainingFragment.kt
+++ app/src/main/java/com/example/runner/ui/plan/TrainingFragment.kt
@@ -1,11 +1,14 @@
-package com.example.runner.ui.plan;
+package com.example.runner.ui.plan
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.example.runner.R
 import com.example.runner.databinding.FragmentTrainingBinding
 
 
@@ -23,7 +26,26 @@ class TrainingFragment : Fragment() {
 
         binding.backButton.setOnClickListener { view?.findNavController()?.navigateUp() }
 
+
+        val listView: ListView = binding.lvMain
+
+        val adapter = ArrayAdapter(
+            requireActivity().application.applicationContext,
+            R.layout.training_list_item, getTrainings()
+        )
+        listView.adapter = adapter
+
         return binding.root
+    }
+
+    private fun getTrainings(): Array<String> {
+        return arrayOf(
+            "Трусцой 02:00 мин.", "Ходьба 02:00 мин.",
+            "Трусцой 02:00 мин.", "Ходьба 01:00 мин.",
+            "Трусцой 05:00 мин.", "Ходьба 02:00 мин.",
+            "Трусцой 05:00 мин.", "Ходьба 02:00 мин.",
+            "Трусцой 02:00 мин.", "Ходьба 01:00 мин.",
+        )
     }
 
     override fun onDestroyView() {
