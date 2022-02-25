e1dba15d9489f8ba83a63482c537cd558c2dcb08
Мельникова Ольга <42916312+olga-alexandrovnaa@users.noreply.github.com>
2022-02-25 11:05:09 +0300

    Update activitys

--- app/src/main/java/com/example/runner/AuthActivity.kt
+++ app/src/main/java/com/example/runner/AuthActivity.kt
@@ -1,7 +1,10 @@
 package com.example.runner
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.example.runner.databinding.ActivityAuthBinding
 
 
@@ -11,7 +14,42 @@ class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        checkPermissions()
+
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    private fun checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_NETWORK_STATE
+            ) != PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.INTERNET
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            val permissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.INTERNET
+            )
+            ActivityCompat.requestPermissions(this, permissions, 0)
+        }
     }
 }
--- app/src/main/java/com/example/runner/MainActivity.kt
+++ app/src/main/java/com/example/runner/MainActivity.kt
@@ -6,12 +6,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.runner.databinding.ActivityMainBinding
+import org.osmdroid.config.Configuration
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val osmConf = Configuration.getInstance()
+
+        val basePath = File(cacheDir.absolutePath, "osmdroid")
+        osmConf.osmdroidBasePath = basePath
+        val tileCache = File(osmConf.osmdroidBasePath.absolutePath, "tile")
+        osmConf.osmdroidTileCache = tileCache
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
