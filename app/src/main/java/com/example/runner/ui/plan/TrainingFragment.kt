e776dc1e94adf6269f7f28b8b41f4f0c67ed3bde
Мельникова Ольга <42916312+olga-alexandrovnaa@users.noreply.github.com>
2022-02-25 11:07:53 +0300

    delete useless imports

--- app/src/main/java/com/example/runner/ui/plan/TrainingFragment.kt
+++ app/src/main/java/com/example/runner/ui/plan/TrainingFragment.kt
@@ -1,7 +1,6 @@
 package com.example.runner.ui.plan
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +8,8 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.example.runner.R
 import com.example.runner.databinding.FragmentTrainingBinding
-import kotlin.math.log
 
 
 class TrainingFragment : Fragment() {
